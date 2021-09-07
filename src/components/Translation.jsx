285c4631e00aa48f14c2e930702832ad4fb2378d
suhyeonglee <sounmind777@gmail.com>
2021-09-08 03:56:46 +0900

    feat: 번역 전, 후 화면 컴포넌트

---
+++ src/components/Translation.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import ContainerStyled from "./shared/Container";
+import TextBox from "./shared/TextBox";
+import SubTitle from "./shared/SubTitle";
+
+import { styled } from "../config/stitches.config";
+
+const Textarea = styled("textarea", {
+  fontSize: "20px",
+  borderRadius: "10px",
+  border: "1px solid $black",
+  wordWrap: "break-word",
+  overflow: "auto",
+  padding: "10px",
+
+  variants: {
+    boxType: {
+      translation: {
+        width: "400px",
+        height: "200px",
+      },
+    },
+    fontSize: {
+      big: { fontSize: "30px" },
+      small: { fontSize: "10px" },
+    },
+  },
+});
+
+export default function Translation({ handleChangeTextInput, textInput }) {
+  return (
+    <ContainerStyled flex="column" border="black">
+      <ContainerStyled flex="column">
+        <SubTitle>번역할 문장</SubTitle>
+        <Textarea
+          boxType="translation"
+          type="text"
+          placeholder="번역할 문장을 입력해주세요."
+          onChange={handleChangeTextInput}
+        />
+      </ContainerStyled>
+      <ContainerStyled flex="column">
+        <SubTitle>번역 결과</SubTitle>
+        <SubTitle>이 번역은 000에 의해 번역된 결과입니다.</SubTitle>
+        <TextBox border="black">
+          {textInput || "변역 결과가 표시됩니다."}
+        </TextBox>
+      </ContainerStyled>
+    </ContainerStyled>
+  );
+}
+
+Translation.propTypes = {
+  textInput: PropTypes.string,
+  handleChangeTextInput: PropTypes.func.isRequired,
+};
+
+Translation.defaultProps = {
+  textInput: "",
+};
