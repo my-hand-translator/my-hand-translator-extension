0378888977c3c31829b0a4c59ebfee017245836d
suhyeonglee <sounmind777@gmail.com>
2021-09-09 18:21:42 +0900

    style: 서버 요청 코드 스타일 변경

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,34 @@
+import { URLS } from "../constants/user";
+
 export const login = async (user) => {
-  return (
-    await fetch(`${process.env.SERVER_URL}/users/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.tokens.access_token}`,
-        "Client-Id": user.clientId,
-      },
-      body: JSON.stringify({
-        email: user.email,
-      }),
-    })
-  ).json();
+  const response = await fetch(process.env.SERVER_URL + URLS.LOGIN, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.tokens.access_token}`,
+      "Client-Id": user.clientId,
+    },
+    body: JSON.stringify({
+      email: user.email,
+    }),
+  });
+
+  return response.json();
 };
 
 export const signup = async (
   { email, name, glossary, clientId, tokens },
   keywords,
 ) => {
-  return (
-    await fetch(`${process.env.SERVER_URL}/users/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${tokens.access_token}`,
-        "Client-Id": clientId,
-      },
-      body: JSON.stringify({ email, name, keywords, glossary }),
-    })
-  ).json();
+  const response = await fetch(process.env.SERVER_URL + URLS.SIGNUP, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${tokens.access_token}`,
+      "Client-Id": clientId,
+    },
+    body: JSON.stringify({ email, name, keywords, glossary }),
+  });
+
+  return response.json();
 };
