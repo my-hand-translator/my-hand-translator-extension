98b4918513cc042b143be001c1e80c7e311bc2ea
aiden <aidencoders@gmail.com>
2021-09-17 15:03:55 +0900

    test: DetailOtherGlossary, EditGlossary 테스트 케이스 추가

--- src/utils/test-utils.jsx
+++ src/utils/test-utils.jsx
@@ -24,23 +24,25 @@ export * from "@testing-library/react";
 
 const user = {
   projectId: "translate-324702",
-  translations: [{
-    nanoId: "qWYBIfLPMsjP1LHNRqlFW",
-    origin: "react",
-    translated: "리액트",
-    url: "vanilla",
-    glossary: {
-      react: "리액트",
+  translations: [
+    {
+      nanoId: "qWYBIfLPMsjP1LHNRqlFW",
+      origin: "react",
+      translated: "리액트",
+      url: "vanilla",
+      glossary: {
+        react: "리액트",
+      },
+      createdAt: "2017-01-01",
     },
-    createdAt: "2017-01-01"
-  }],
+  ],
   glossary: {
-    react: "리액트"
+    react: "리액트",
   },
   glossaryId: "6141bd1c2a11e5b51320bac1",
   isServerOn: false,
   name: "aidencoders",
-  email: "aidencoders@gmail.com"
+  email: "aidencoders@gmail.com",
 };
 
 export { render, user };
